62af1361e8b8136d705ec130fc140884f7a5417a
Dev-Winty <lkj1688@daum.net>
2020-09-16 08:33:03 +0900

    initial commit

---
+++ SignIn.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Maeily_Windows
+{
+    /// <summary>
+    /// SignIn.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SignIn : Window
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Widget.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Maeily_Windows
+{
+    /// <summary>
+    /// Widget.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Widget : Window
+    {
+        public Widget()
+        {
+            InitializeComponent();
+        }
+    }
+}
