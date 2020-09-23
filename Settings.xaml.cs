818e1df57513e41a2ad74f7409225199f7752e01
LimKyeongJoon <kyeongjoon16881688@gmail.com>
2020-09-23 20:55:11 +0900

    설정 UI 구성
    설정의 UI를 구성했다.

--- Settings.xaml.cs
+++ Settings.xaml.cs
@@ -1,11 +1,24 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace Maeily_Windows
 {
     /// <summary>
     /// Settings.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class Settings : Window
+    public partial class Settings : Page
     {
         public Settings()
         {
