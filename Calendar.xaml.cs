704189078af403c99b4087ff020c9566dceb5367
Dev-Winty <lkj1688@daum.net>
2020-09-28 20:56:37 +0900

    (재)Calendar 기능 구현
    캘린더 기능 구현

--- Calendar.xaml.cs
+++ Calendar.xaml.cs
@@ -20,9 +20,79 @@ namespace Maeily_Windows
     /// </summary>
     public partial class Calendar : Page
     {
+        private DateTime dateTime = DateTime.UtcNow;
+
         public Calendar()
         {
             InitializeComponent();
+
+            Loaded += new RoutedEventHandler(Calendar_Loaded);
+        }
+
+        private void Calendar_Loaded(object sender, RoutedEventArgs args)
+        {
+            int i;
+
+            LbTitleDate.Content = dateTime.ToString("yyyy년 MM월");
+
+            UGridCalendar.FirstColumn = CheckDateColumn();
+
+            for (i = 1; i <= DateTime.DaysInMonth(dateTime.Year, dateTime.Month); i++)
+            {
+                Button button = new Button()
+                {
+                    Width = 25,
+                    Height = 25,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Tag = i.ToString(),
+                    Background = Brushes.Transparent,
+                    BorderBrush = Brushes.Transparent
+                };
+                TextBlock textBlock = new TextBlock()
+                {
+                    Text = i.ToString(),
+                };
+                textBlock.FontWeight = FontWeights.Bold;
+                button.Content = textBlock;
+                UGridCalendar.Children.Add(button);
+            }
+        }
+
+        private int CheckDateColumn()
+        {
+            int first = 0;
+            switch (new DateTime(dateTime.Year, dateTime.Month, 1).DayOfWeek.ToString())
+            {
+                case "Sunday":
+                    first = 0;
+                    break;
+
+                case "Monday":
+                    first = 1;
+                    break;
+
+                case "Tuesday":
+                    first = 2;
+                    break;
+
+                case "Wednesday":
+                    first = 3;
+                    break;
+
+                case "Thursday":
+                    first = 4;
+                    break;
+
+                case "Friday":
+                    first = 5;
+                    break;
+
+                case "Saturday":
+                    first = 6;
+                    break;
+            }
+            return first;
         }
     }
 }
