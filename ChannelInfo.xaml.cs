814f1d69a0fe87d31c6c1a0a29bc47e304ac6c58
Dev-Winty <lkj1688@daum.net>
2020-10-05 15:56:24 +0900

    ChannelInfo 유저 표시 기능 추가
    ChannelInfo에 유저 정보가 표시되도록 함

--- ChannelInfo.xaml.cs
+++ ChannelInfo.xaml.cs
@@ -23,6 +23,39 @@ namespace Maeily_Windows
         public ChannelInfo()
         {
             InitializeComponent();
+            Loaded += new RoutedEventHandler(ChannelInfo_Loaded);
+        }
+
+        private void ChannelInfo_Loaded(object sender, RoutedEventArgs args)
+        {
+            AddUserProfile(new BitmapImage(new Uri("Resources/AddBtn.png", UriKind.Relative)), "1117 임경준");
+            AddUserProfile(new BitmapImage(new Uri("Resources/AddBtn.png", UriKind.Relative)), "1117 임경준");
+        }
+
+        private void AddUserProfile(ImageSource imgSrc, string userInfo)
+        {
+            StackPanel stackPanel = new StackPanel()
+            {
+                Orientation = Orientation.Horizontal,
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            Image image = new Image();
+            image.Source = imgSrc;
+            image.Margin = new Thickness(0, 10, 30, 10);
+            image.Width = 40;
+            image.Height = 40;
+            TextBlock textBlock = new TextBlock()
+            {
+                FontWeight = FontWeights.Bold,
+                Text = userInfo,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                FontSize = 15
+            };
+            stackPanel.Children.Add(image);
+            stackPanel.Children.Add(textBlock);
+            Userlist.Children.Add(stackPanel);
         }
     }
 }
