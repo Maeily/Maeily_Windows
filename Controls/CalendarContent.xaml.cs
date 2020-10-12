1ca38c9175be79559589af8c5b841ba6a6309d07
Dev-Winty <lkj1688@daum.net>
2020-10-12 11:30:40 +0900

    uc CalendarContent 추가
    userControl CalendarContent 추가

---
+++ Controls/CalendarContent.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Maeily_Windows.Controls
+{
+    /// <summary>
+    /// CalendarContent.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class CalendarContent : UserControl
+    {
+        private int important = 1;
+        private string content = string.Empty;
+
+        public CalendarContent(int important, string content)
+        {
+            InitializeComponent();
+            Loaded += CalendarContent_Loaded;
+
+            this.important = important;
+            this.content = content;
+        }
+
+        private void CalendarContent_Loaded(object sender, RoutedEventArgs e)
+        {
+            retangle.Fill = Brushes.Red;
+            TxtContent.Text = content;
+        }
+    }
+}
