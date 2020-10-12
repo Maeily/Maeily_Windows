05dc230da4c890ecd650d37e7c22f2a91586e7cb
Dev-Winty <lkj1688@daum.net>
2020-10-12 19:28:59 +0900

    Settings 내 버튼 Template 변경
    Settings 내 버튼 Template 변경

--- Settings.xaml.cs
+++ Settings.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
 
 namespace Maeily_Windows
 {
@@ -12,19 +10,6 @@ namespace Maeily_Windows
         public Settings()
         {
             InitializeComponent();
-            BtnSubmit.MouseEnter += BtnSubmit_MouseEnter;
-            BtnSubmit.MouseLeave += BtnSubmit_MouseLeave;
-        }
-
-        private void BtnSubmit_MouseLeave(object sender, MouseEventArgs e)
-        {
-            var Bc = new BrushConverter();
-            BtnSubmit.BorderBrush = (Brush)Bc.ConvertFrom("#F1F1F1");
-        }
-
-        private void BtnSubmit_MouseEnter(object sender, MouseEventArgs e)
-        {
-            BtnSubmit.BorderBrush = Brushes.Blue;
         }
     }
 }
