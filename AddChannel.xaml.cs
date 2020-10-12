dfc76557969afd59a34d31e003c51060d7b43416
Dev-Winty <lkj1688@daum.net>
2020-10-12 20:19:48 +0900

    AddChannel 내 채널 이미지가 변경되도록 함
    AddChannel 내 채널 이미지가 변경되도록 함

--- AddChannel.xaml.cs
+++ AddChannel.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace Maeily_Windows
 {
@@ -11,6 +15,19 @@ namespace Maeily_Windows
         {
             InitializeComponent();
             CbColor.SelectionChanged += CbColor_SelectionChanged;
+            ImgChannel.MouseDown += ImgChannel_MouseDown;
+        }
+
+        private void ImgChannel_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JPEG File (*.jpg)|*.jpg|PNG File (*.png)|*.png|BITMAP (*bmp)|*.bmp";
+            openFileDialog.ShowDialog();
+
+            if (openFileDialog.FileName != "")
+            {
+                ImgChannel.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
         }
 
         private void CbColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
