713e842da2eb0cca45da5f6f9978a95cee39c07d
Dev-Winty <lkj1688@daum.net>
2020-10-12 19:55:07 +0900

    AddChannel UI 변경
    AddChannel UI 변경

--- AddChannel.xaml.cs
+++ AddChannel.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace Maeily_Windows
 {
@@ -23,6 +10,12 @@ namespace Maeily_Windows
         public AddChannel()
         {
             InitializeComponent();
+            CbColor.SelectionChanged += CbColor_SelectionChanged;
+        }
+
+        private void CbColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CbColor.Foreground = (CbColor.SelectedItem as ComboBoxItem).Foreground;
         }
     }
 }
