7de51750fdd7f31d970b881a9f6e37cf19b0312a
Dev-Winty <lkj1688@daum.net>
2020-10-14 20:38:56 +0900

    채널 생성 시 채널 정보 파일이 생성되도록 함
    채널 생성 후 채널 정보 파일을 실행 파일과 같은 폴더 안 channel 폴더 안에 채널 정보에 대한 파일이 생성되도록 함

--- AddChannel.xaml.cs
+++ AddChannel.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
@@ -16,6 +19,63 @@ namespace Maeily_Windows
             InitializeComponent();
             CbColor.SelectionChanged += CbColor_SelectionChanged;
             ImgChannel.MouseDown += ImgChannel_MouseDown;
+            BtnSubmit.Click += BtnSubmit_Click;
+        }
+
+        private void BtnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            FileInfo fileInfo = new FileInfo("Channel/" + TbChannelName.Text + ".txt");
+            DirectoryInfo directory = new DirectoryInfo("Channel");
+            FileStream fs;
+            string isPublic = "False";
+
+            if (TbChannelName.Text == "" || CbColor.Text == "")
+            {
+                MessageBox.Show("입력 칸을 채워주세요!", "메일리");
+                return;
+            }
+
+            if (directory.Exists == false)
+            {
+                directory.Create();
+            }
+
+            if (fileInfo.Exists == false)
+            {
+                fs = fileInfo.Create();
+                fs.Close();
+            }
+            else
+            {
+                MessageBox.Show("이미 존재하는 채널입니다!", "메일리");
+                TbChannelName.Text = null;
+                CbColor.SelectedIndex = 0;
+                ChkIsPublic.IsChecked = false;
+                return;
+            }
+
+            if (ChkIsPublic.IsChecked == true)
+            {
+                isPublic = "True";
+            }
+
+            StreamWriter writer = new StreamWriter("Channel/" + TbChannelName.Text + ".txt");
+            JObject jObject = new JObject();
+
+            jObject.Add("channel_name", TbChannelName.Text);
+            jObject.Add("channel_code", "채널 코드");
+            jObject.Add("color", CbColor.Text);
+            jObject.Add("isPublic", isPublic);
+
+            writer.Write(JsonConvert.SerializeObject(jObject));
+            writer.Flush();
+            writer.Close();
+
+            TbChannelName.Text = null;
+            CbColor.SelectedItem = null;
+            ChkIsPublic.IsChecked = false;
+            MessageBox.Show("채널이 생성되었습니다!", "메일리");
+            return;
         }
 
         private void ImgChannel_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -32,7 +92,9 @@ namespace Maeily_Windows
 
         private void CbColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            CbColor.Foreground = (CbColor.SelectedItem as ComboBoxItem).Foreground;
+            if (CbColor.SelectedItem != null) {
+                CbColor.Foreground = (CbColor.SelectedItem as ComboBoxItem).Foreground;
+            }
         }
     }
 }
