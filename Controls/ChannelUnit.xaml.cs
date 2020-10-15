96a9feb07bbbc40e9b7c69112bc06c6f625dc0eb
Dev-Winty <lkj1688@daum.net>
2020-10-15 12:31:13 +0900

    uc ChannelUnit 추가

---
+++ Controls/ChannelUnit.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Maeily_Windows.Controls
+{
+    public partial class ChannelUnit : UserControl
+    {
+        private string channelName = string.Empty;
+        public ChannelUnit(String channelName)
+        {
+            InitializeComponent();
+            Loaded += ChannelUnit_Loaded;
+
+            this.channelName = channelName;
+        }
+
+        private void ChannelUnit_Loaded(object sender, RoutedEventArgs e)
+        {
+            TxtChannelName.Text = channelName;
+        }
+    }
+}
