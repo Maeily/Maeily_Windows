5ec40dea8aae9a0ea6611d7a0f25abe9a64e42f3
prs176 <>
2020-10-16 20:13:30 +0900

    채널 일정추가

--- Channel_Settings.xaml.cs
+++ Channel_Settings.xaml.cs
@@ -20,9 +20,54 @@ namespace Maeily_Windows
     /// </summary>
     public partial class Channel_Settings : Page
     {
+
         public Channel_Settings()
         {
             InitializeComponent();
         }
+
+        public bool isalready = false;
+
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtbox.Visibility == Visibility.Hidden)
+            {
+                txtbox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                AddCalendar(txtbox.Text);
+                txtbox.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void AddCalendar(string name)
+        {
+            isalready = false;
+            StackPanel stackPanel = new StackPanel()
+            {
+                Orientation = Orientation.Horizontal,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Width = 200,
+                Margin = new Thickness(10)
+            };
+            Rectangle rectangle = new Rectangle();
+            rectangle.Width = 10;
+            rectangle.Height = 10;
+            rectangle.Margin = new Thickness(60, 0, 0, 0);
+            rectangle.Fill = Brushes.Black;
+            TextBlock textBlock = new TextBlock()
+            {
+                VerticalAlignment = VerticalAlignment.Center,
+                Margin = new Thickness(10, 0, 0, 0),
+                Text = name
+            };
+            stackPanel.Children.Add(rectangle);
+            stackPanel.Children.Add(textBlock);
+            CalendarList.Children.Add(stackPanel);
+        }
+
     }
 }
