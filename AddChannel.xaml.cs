a02dbceb161367923602455d5e43fd61c19847e8
Dev-Winty <lkj1688@daum.net>
2020-10-18 16:00:27 +0900

    AddChannel 에서 Channel을 추가하면 Channel/Schedule 폴더 내 Schedules 파일을 만들도록 함
    AddChannel 에서 Channel을 추가하면 Channel/Schedule 폴더 내 Schedules 파일을 만들도록 함.  {[]}

--- AddChannel.xaml.cs
+++ AddChannel.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.IO;
 using System.Windows;
@@ -14,22 +15,37 @@ namespace Maeily_Windows
     /// </summary>
     public partial class AddChannel : Page
     {
-        private FileInfo fileInfo; 
-        OpenFileDialog openFileDialog = new OpenFileDialog();
+        private FileInfo fileInfo;
+        private OpenFileDialog openFileDialog = new OpenFileDialog();
+
         public AddChannel()
         {
             InitializeComponent();
             CbColor.SelectionChanged += CbColor_SelectionChanged;
             ImgChannel.MouseDown += ImgChannel_MouseDown;
             BtnSubmit.Click += BtnSubmit_Click;
+            Loaded += AddChannel_Loaded;
+        }
+
+        private void AddChannel_Loaded(object sender, RoutedEventArgs e)
+        {
+            DirectoryInfo directory = new DirectoryInfo("Channel/Schedules");
+
+            if (!directory.Exists)
+            {
+                directory.Create();
+            }
         }
 
         private void BtnSubmit_Click(object sender, RoutedEventArgs e)
         {
             string file = string.Empty;
             fileInfo = new FileInfo("Channel/" + TbChannelName.Text + ".txt");
+            FileInfo fileSchedules =
+                new FileInfo("Channel/Schedules/" + TbChannelName.Text + ".txt");
             DirectoryInfo directory = new DirectoryInfo("Channel");
             DirectoryInfo directoryRes = new DirectoryInfo("Channel/Resources");
+            DirectoryInfo directorySch = new DirectoryInfo("Channel/Schedules");
             FileStream fs;
             string isPublic = "False";
 
@@ -37,6 +53,11 @@ namespace Maeily_Windows
             {
                 MessageBox.Show("입력 칸을 채워주세요!", "메일리");
                 return;
+            }
+
+            if (!directorySch.Exists)
+            {
+                directory.Create();
             }
 
             if (directory.Exists == false)
@@ -52,6 +73,8 @@ namespace Maeily_Windows
             if (fileInfo.Exists == false)
             {
                 fs = fileInfo.Create();
+                fs.Close();
+                fs = fileSchedules.Create();
                 fs.Close();
             }
             else
@@ -69,7 +92,11 @@ namespace Maeily_Windows
             }
 
             StreamWriter writer = new StreamWriter("Channel/" + TbChannelName.Text + ".txt");
+            StreamWriter writerSch =
+                new StreamWriter("Channel/Schedules/" + TbChannelName.Text + ".txt");
             JObject jObject = new JObject();
+            JArray jArray = new JArray();
+            JObject JArrayObject = new JObject();
 
             jObject.Add("channel_name", TbChannelName.Text);
             jObject.Add("channel_code", "채널 코드");
@@ -93,6 +120,10 @@ namespace Maeily_Windows
             writer.Write(JsonConvert.SerializeObject(jObject));
             writer.Flush();
             writer.Close();
+            JArrayObject.Add(new JProperty("Schedules", jArray));
+            writerSch.Write(JsonConvert.SerializeObject(JArrayObject));
+            writerSch.Flush();
+            writerSch.Close();
 
             TbChannelName.Text = null;
             CbColor.SelectedItem = null;
@@ -114,7 +145,8 @@ namespace Maeily_Windows
 
         private void CbColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (CbColor.SelectedItem != null) {
+            if (CbColor.SelectedItem != null)
+            {
                 CbColor.Foreground = (CbColor.SelectedItem as ComboBoxItem).Foreground;
             }
         }
