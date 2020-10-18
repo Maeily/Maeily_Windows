f1e2f32df55481bd22cf8c175d7d2b8281adc45d
Dev-Winty <lkj1688@daum.net>
2020-10-18 12:39:45 +0900

    Channel 에서 각각의 채널들의 Border 색상을 대표 색상으로 표시되도록 함
    Channel 에서 각각의 채널들의 Border 색상을 대표 색상으로 표시되도록 함

--- Controls/ChannelUnit.xaml.cs
+++ Controls/ChannelUnit.xaml.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
+using Brush = System.Windows.Media.Brush;
+using Brushes = System.Windows.Media.Brushes;
 
 namespace Maeily_Windows.Controls
 {
     public partial class ChannelUnit : UserControl
     {
         private string channelName = string.Empty;
+        private JObject JObject;
 
         public ChannelUnit(String channelName)
         {
@@ -32,7 +36,49 @@ namespace Maeily_Windows.Controls
             if (fileInfo.Exists)
             {
                 ImgChannelImage.Source = new BitmapImage(new Uri(fileInfo.FullName, UriKind.Absolute));
+                JObject = JObject.Parse(System.IO.File.ReadAllText(@"Channel/" + channelName + ".txt"));
+                var converter = new System.Windows.Media.BrushConverter();
+                BtnChannelUnit.Background = CheckColor(JObject["color"].ToString());
             }
+        }
+
+        private Brush CheckColor(string src)
+        {
+            var converter = new System.Windows.Media.BrushConverter();
+            Brush result = (Brush)converter.ConvertFromString("#B7DE4B");
+
+            switch (src)
+            {
+                case "빨강":
+                    result = Brushes.Red;
+                    break;
+
+                case "주황":
+                    result = Brushes.Orange;
+                    break;
+
+                case "노랑":
+                    result = Brushes.Yellow;
+                    break;
+
+                case "초록":
+                    result = Brushes.Green;
+                    break;
+
+                case "파랑":
+                    result = Brushes.Blue;
+                    break;
+
+                case "남색":
+                    result = Brushes.Indigo;
+                    break;
+
+                case "보라":
+                    result = Brushes.Purple;
+                    break;
+            }
+
+            return result;
         }
     }
 }
