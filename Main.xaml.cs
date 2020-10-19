6bb90269fc46f5f57481dea3923053f4b95a4c89
Dev-Winty <lkj1688@daum.net>
2020-10-19 20:04:33 +0900

    Main에 급식 글씨의 FontWeight 을 Bold로 변경
    Main에 급식 글씨의 FontWeight 을 Bold로 변경

--- Main.xaml.cs
+++ Main.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Maeily_Windows
 {
@@ -26,14 +15,16 @@ namespace Maeily_Windows
         public Main()
         {
             InitializeComponent();
+            text.FontWeight = FontWeights.Bold;
+            text.HorizontalAlignment = HorizontalAlignment.Center;
             Meal();
         }
-        
+
         private void Meal()
         {
             string now = DateTime.Now.ToString("HH");
 
-            if(double.Parse(now) >= 19 && double.Parse(now) < 8)
+            if (double.Parse(now) >= 19 && double.Parse(now) < 8)
             {
                 myPage = 2;
             }
